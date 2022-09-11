03d4a7269d697536794375b7e896f93a4b5b5ba0
atul457m <as377183@gmail.com>
2022-09-12 01:35:38 +0530

    up

--- resources/views/public/fill_up_form/form.blade.php
+++ resources/views/public/fill_up_form/form.blade.php
@@ -10,21 +10,21 @@
 @endsection
 @section('content')
 <div class="formContainer row px-2">
+  @if ($errors->any())
   <div class="card">
-    @if ($errors->any())
     <div class="alert alert-danger p-1">
       {{$errors->all()[0]}}
     </div>
-    @endif
   </div>
+  @endif
 
+  @if(Session::has("error"))
   <div class="card pt-1">
-    @if(Session::has("error"))
     <div class="alert alert-danger p-1">
       {{Session::get('error')}}
     </div>
-    @endif
   </div>
+  @endif
 
   @if(isset($form))
   <input 
@@ -32,13 +32,71 @@
     id="form_json" 
     value="{{$form['form_json']}}"/>
 
+    <!-- {{print_r($form)}} -->
+
   <div class="card col-12 page_header">
-    <div class="card-header border-bottom">
-      {{$form["form_name"]}}
+    <!-- <div class="card_header">
+      {{$form["user_ref"]}}
+    </div> -->
+
+    <div class="comp_details">
+      <div class="card_header">
+          {{$form["comp_name"]}} 
+      </div>
+      <div class="form_header_fields">
+      <span class="values">{{$form["comp_addr"]}}</span>
+      </div>
     </div>
+        
+    <div class="form_header_fields">
+      <label>Customer care no :</label>
+      <span class="values">
+        {{$form["comp_care_no"]}}
+      </span>
+    </div>
+    
+    <div class="form_header_fields">
+      <label>Survey creator :</label>
+      <span class="values">
+        {{$form["name"]}}
+      </span>
+    </div>
+    
+    <div class="form_header_fields">
+      <label>Phone no :</label>
+      <span class="values">
+        {{$form["phone_no"]}}
+      </span>
+    </div>
+    
+    <div class="form_header_fields">
+      <label>Date :</label>
+      <span class="values">
+        {{$form["start_date"]}}
+      </span>
+    </div>
+
   </div>
 
-  <form method="post" action="" class="col-12 px-0">
+  <form method="post" action="{{url('saveform')}}" class="col-12 px-0" onsubmit="return getFormData()">
+      @csrf
+      <input
+        type="hidden"
+        id="data_filled"
+        name="data_filled"/>
+
+      <input
+        type="hidden"
+        id="user_ref"
+        name="user_ref"
+        value='{{$form["user_ref"]}}'/>
+
+      <input
+        type="hidden"
+        id="survey_form_ref"
+        name="survey_form_ref"
+        value='{{$form["id"]}}'/>
+
       <div class="fb-render">
         <textarea id="fb-template" class="d-none">
             <form-template>
@@ -46,6 +104,13 @@
         </textarea>
       </div>
   </form>
+
+  <div class="col-12 mt-3 px-0">
+    <div class="fw-bold">
+      Batch no: {{$form["batch_no"]}}
+    </div>
+  </div>
+
   @endif
 </div>
 @endsection
