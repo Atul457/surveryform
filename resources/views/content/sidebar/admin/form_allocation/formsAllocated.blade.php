d0ec6aed77e257588d60ef00bbb627283d350b32
atul457m <as377183@gmail.com>
2022-09-18 17:01:11 +0530

    up

--- resources/views/content/sidebar/admin/form_allocation/formsAllocated.blade.php
+++ resources/views/content/sidebar/admin/form_allocation/formsAllocated.blade.php
@@ -101,7 +101,7 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                Are you sure you want to deallocate this form for the user?
+                Are you sure you want to deallocate this form for the employee?
                 <form 
                     action="{{url('deallocateform')}}" 
                     id="deallocationForm" 
